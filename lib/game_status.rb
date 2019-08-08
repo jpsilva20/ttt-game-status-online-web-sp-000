760fcececacc881bbed859a2b92afead39ea0daa
Jennifer Silva <jennifersilva20@gmail.com>
2019-08-08 20:28:53 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -20,9 +20,8 @@ def won?(board)
       return win_combination
     elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
       return win_combination
-    end 
-  else 
-    return false
+    else 
+      return false
   }
 end 
 
