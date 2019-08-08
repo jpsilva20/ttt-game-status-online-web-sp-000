92e15a49b596892f3ec3026a8813b721448fab27
Jennifer Silva <jennifersilva20@gmail.com>
2019-08-08 19:40:17 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,4 +4,14 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
-WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8], ]
+WIN_COMBINATIONS = [[0,1,2], [3,4,5], [6,7,8],[0,3,6], []
+
+
+
+
+[0,3,6])
+      expect(WIN_COMBINATIONS).to include_array([1,4,7])
+      expect(WIN_COMBINATIONS).to include_array([2,5,8])
+      expect(WIN_COMBINATIONS).to include_array([0,4,8])
+      expect(WIN_COMBINATIONS).to include_array([6,4,2])
+    end
